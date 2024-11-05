08ae13ebb17021cbaedb09b2f7eca0a6e525a706
Verdy <verdywahyudi321@gmail.com>
2024-11-05 20:00:59 +0700

    add api

---
+++ src/app/api/test-api/route.ts
@@ -0,0 +1,27 @@
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+
+    const authorization =req.headers.get('authorization')
+       
+    const data = {
+        name: 'John',
+        age: 30
+    }
+
+    if (!authorization) {    
+        return NextResponse.json({error: 'No Header'}, {status:500})
+    }
+
+    if (authorization != '123123') {
+        return NextResponse.json({error: 'wrong password'}, {status:400})
+    }
+
+    else return new Response(JSON.stringify(data), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+}
