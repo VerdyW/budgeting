a7d08dcd9a5d7def10f2f49955df02d5042649ea
unknown <verdy@mail.com>
2024-11-07 17:50:03 +0700

    removed console.log

--- src/app/api/transactions/expenses/route.ts
+++ src/app/api/transactions/expenses/route.ts
@@ -40,8 +40,6 @@ export async function GET(request:NextRequest) {
 
     const data = await Expenses.find({})
 
-    console.log(data)
-
     if (!authorization) {    
         return NextResponse.json({error: 'No Header'}, {status:500})
     }
